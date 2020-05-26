5490078a2ef890f706cd5184deaafe68ea7e0964
mogunnell <63001400+mogunnell@users.noreply.github.com>
2020-05-26 14:36:11 -0400

    win

--- chat2/script.js
+++ chat2/script.js
@@ -3,17 +3,23 @@ login(start, recieveMessage);
 
 //This function runs after you have logged in. Your user information is passed to you in the variable 'user'
 function start(user) {
-	console.log(user);
-	document.getElementById("sendMessage").addEventListener("click", send);
+    console.log(user);
+    document.getElementById("send-message").addEventListener("click", send);
 }
 
 //This function runs every time you recieve a message. It will run for every message already in the database.
 //The message information is passed to you in the variable 'message'
 function recieveMessage(message) {
-	console.log(message);
+    var name = message.name;
+    var content = message.content;
+
+	var nameAndText = message.name + ": " + message.content;
+
+	document.getElementById("display").innerHTML += nameAndText;
 }
 
 //The sendMessage('text') function will send a message from you to the database.
 function send() {
-	sendMessage("Hello World");
+    var message = document.getElementById("message").value;
+    sendMessage(message);
 }
